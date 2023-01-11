9dfcd368567b94b5114e49c04f04d9367f34e427
Mike Melchior <mike.melchior@outlook.com>
2023-01-11 16:22:23 -0500

    create ui with DOM manipulation using pre-existing rps script

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 
 continuing to de-rust, skimming over already completed projects and redoing them to practice.
+
+
+adding ui to rps from separate branch (rps-ui)
--- script.js
+++ script.js
@@ -1,27 +1,31 @@
-gameMoves = ['rock', 'paper', 'scissors'];
+const gameMoves = ['rock', 'paper', 'scissors'];
+let move;
+let playerScore = 0;
+let computerScore = 0;
 
-getComputerChoice = () => {
+// random computer selection
+const getComputerChoice = () => {
     let choice = Math.floor(Math.random(1)*3);
     let move = gameMoves[choice];
     return move;
 };
 
-const getPlayerChoice = () => {
-    let choice = prompt('Select rock, paper or scissors').toLowerCase();
-    while (choice !== 'rock' && choice !== 'scissors' && choice !== 'paper') {
-        alert('Please make sure your selection is spelled correctly');
-        choice = prompt('Select rock, paper or scissors').toLowerCase();
-    }
-    return choice
+// functions to recognize player move on button click
+const setRock = () => {
+    move = 'rock';
+};
+const setPaper = () => {
+    move = 'paper';
+};
+const setScissors = () => {
+    move = 'scissors';
 };
 
-let playerScore = 0;
-let computerScore = 0;
-
-playRound = () => {
+// function to decide winner of single round and distribute winning points accordingly
+const playRound = () => {
 
     let computerMove = getComputerChoice();
-    let playerMove = getPlayerChoice();
+    let playerMove = move;
 
     alert('Computer chose: ' + computerMove + ' and you chose: ' + playerMove);
     if(computerMove == playerMove) {
@@ -40,24 +44,98 @@ playRound = () => {
     }
 }
 
-game = () => {
-    playerScore = 0;
-    computerScore = 0;
-    while (playerScore < 3 && computerScore < 3) {
-        alert('player: ' + playerScore + ' computer: ' + computerScore);
-        playRound();
-    }
-    alert('player: ' + playerScore + ' computer: ' + computerScore);
-    if (playerScore === 3){
-        alert('You win!');
-        return 'winner = player';
-    }
-    if (computerScore === 3) {
-        alert('Computer wins!');
-        return 'winner = computer';
-    }
+//function to keep score card up to date
+const updateScore = () => {
+    const playerScoreSelector = document.querySelector('.player');
+    const computerScoreSelector = document.querySelector('.computer');
+    playerScoreSelector.textContent = playerScore;
+    computerScoreSelector.textContent = computerScore;
+}
+
+// function to check if either player has five points to determine if game has been won
+const checkForWinner = () => {
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore == 5) {
+            alert('You win!');
+        } else {
+            alert('Computer wins');
+        };
+        playerScore = 0;
+        computerScore = 0;
+    };
+    return
 }
 
 
-const playButton = document.querySelector('.play-button');
-playButton.addEventListener('click', game)
+// 
+// 
+//                 DOM MANIPULATION  ~~~~~~~~~~~~~~~~~~~~~~~~
+// 
+// 
+
+
+
+
+
+
+
+
+
+const addMoveSelectionButton = (text, classToAdd) => {
+    const buttonContainer = document.querySelector('.button-container');
+    const button = document.createElement('button');
+    button.classList.add(`rps-button`, `${classToAdd}`);
+    button.textContent = text;
+    buttonContainer.appendChild(button);
+}
+
+addMoveSelectionButton('rock', 'rock');
+addMoveSelectionButton('paper', 'paper');
+addMoveSelectionButton('scissors', 'scissors');
+
+
+const rockButton = document.querySelector('.rock');
+rockButton.addEventListener('click', setRock);
+rockButton.addEventListener('click', playRound);
+
+const paperButton = document.querySelector('.paper');
+paperButton.addEventListener('click', setPaper);
+paperButton.addEventListener('click', playRound);
+
+const scissorsButton = document.querySelector('.scissors');
+scissorsButton.addEventListener('click', setScissors);
+scissorsButton.addEventListener('click', playRound);
+
+const playButtons = document.querySelectorAll('.rps-button');
+playButtons.forEach(button => {
+    addEventListener('click', updateScore);
+});
+
+playButtons.forEach(button => {
+    addEventListener('click', checkForWinner)
+})
+
+const scoreCard = document.querySelector('.score-card');
+
+const buildScoreCard = (player, scoreCardTitle, score) => {
+    const div = document.createElement('div');
+    div.classList.add('score-container');
+
+    const p = document.createElement('p');
+    p.classList.add('score-text');
+    p.textContent = scoreCardTitle;
+
+    const p2 = document.createElement('p');
+    p2.classList.add('score-text',`${player}`);
+    p2.textContent = score;
+
+    div.appendChild(p);
+    div.appendChild(p2);
+
+    return div
+}
+
+scoreCard.appendChild(buildScoreCard('player', 'Player\'s Score: ', `${playerScore}`));
+scoreCard.appendChild(buildScoreCard('computer', 'Computer\'s Score: ', `${computerScore}`))
+
+
